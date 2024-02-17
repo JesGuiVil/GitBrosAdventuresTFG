d8567e36c89890b4b81bf81be17206bb1bbc8468
jesus <ardijesus@gmail.com>
2024-02-17 07:18:17 +0100

    dolor

--- My project/Assets/Scripts/CanvasScript.cs
+++ My project/Assets/Scripts/CanvasScript.cs
@@ -10,6 +10,7 @@ public class CanvasScript : MonoBehaviour
     {
         Vector3 position = transform.position;
         position.x = Rogue.transform.position.x;
+        position.y = Rogue.transform.position.y;
         transform.position = position;
     }
 }

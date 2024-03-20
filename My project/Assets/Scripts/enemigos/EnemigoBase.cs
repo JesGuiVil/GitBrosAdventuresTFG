6fce4c198c72fcba2199a0315caca05f0397f97d
Kike Bellido <ebellidoperejil2@gmail.com>
2024-03-20 10:57:52 +0100

    animaciones muerte y hurt terminadas

--- My project/Assets/Scripts/enemigos/EnemigoBase.cs
+++ My project/Assets/Scripts/enemigos/EnemigoBase.cs
@@ -71,7 +71,6 @@ public class EnemigoBase : MonoBehaviour
                     anim.SetTrigger("meleAtack");
                 }
             }
-            
         }
         if (PlayerInSightDistancia())
         {

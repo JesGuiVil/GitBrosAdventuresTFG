9e41e25666f5a0627b2b382c53fc11c751ff2555
Yeraaay <moraledacabrerayerai@gmail.com>
2024-05-26 22:14:37 +0200

    configurando daga archer

--- My project/Assets/Scripts/personajes/archer/Archer.cs
+++ My project/Assets/Scripts/personajes/archer/Archer.cs
@@ -7,4 +7,19 @@ public class Archer : PersonajeBase
     public bool tengoAlgo = false;
     public bool Ablado = false;
     public bool algoEntregada = false;
+
+    protected override void Update()
+    {
+        // Llama al método Update de la clase base
+        base.Update();
+        if (!isDead)
+        {
+            // Agrega la lógica específica del Rogue aquí
+            if (Input.GetKeyDown(KeyCode.Space) && cooldownTimer >= cooldownCerca)
+            {
+                animator.SetTrigger("ataquemelee");
+                cooldownTimer = 0f;
+            }
+        }
+    }
 }
